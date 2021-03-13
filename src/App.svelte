4ab37f2edefe86ad3a612e9c7bfca4950f884535
Michael Vogt <info@michaelvogt.eu>
2021-03-13 12:52:54 +0100

    Receive the headless peerid from service discovery

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,102 @@
 <script>
-  import {onMount} from 'svelte';
+    import {onMount} from 'svelte';
 
-  onMount(() => {
-  });
+    import {getServicesAtLocation} from 'ssd-access';
+
+    import {getCurrentLocation} from "./utilities";
+
+
+    // The peer ID of the headless client
+    let headlessPeerId;
+
+    // Peer ID of this app instance
+    let localPeerId;
+
+    // Peer IDs of connected peers
+    let connectedPeers;
+
+    // Value to be synced between the peers
+    let valueToSync
+
+
+    // Runs at startup of the app
+    onMount(() => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+
+        if (urlParams.has('peerid')) {
+            // Defines the headless client
+            // This ID needs to be registered with service discovery
+            localPeerId = urlParams.get('peerid');
+
+            // Register with Signaling Server
+            connectToSignalingServer();
+        } else {
+            // Get headless client peer id from service discovery
+            requestHeadlessPeerId()
+                .then(peerId => {
+                    headlessPeerId = peerId;
+                    connectToHeadlessClient();
+                })
+                .catch(error => console.log(error));
+
+            // When list of peers currently connected to headless client received -- register event
+            connectToPeers();
+        }
+    });
+
+
+    function connectToSignalingServer() {
+
+    }
+
+    function requestHeadlessPeerId() {
+        let found = false;
+
+        return new Promise((resolve, reject) => {
+            getCurrentLocation()
+                .then(currentLocation => getServicesAtLocation(currentLocation.regionCode, currentLocation.h3Index))
+                .then(data => {
+                    data.forEach(ssr => {
+                        ssr.services
+                            .forEach(service => {
+                                if (service.type === 'p2p-master') {
+                                    found = true;
+
+                                    console.log(service.description);
+                                    resolve(service.description);
+                                }
+                            })
+                    });
+
+                    if (!found) {
+                        reject('No headless peer id found')
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject(error);
+                });
+        });
+    }
+
+    function connectToHeadlessClient() {
+
+    }
+
+    function connectToPeers() {
+        // Connect to peers with id received from headless client
+    }
 </script>
 
 <style>
-  :global(body) {
-    margin: 0;
-    font-family: Arial, Helvetica, sans-serif;
-  }
+    :global(body) {
+        margin: 0;
+        font-family: Arial, Helvetica, sans-serif;
+    }
 </style>
 
 <div>
-<h1>p2p test</h1>
+    <h1>p2p test</h1>
+    <input type="number" bind:value={valueToSync}/>
 </div>
---
+++ src/utilities.js
@@ -0,0 +1,45 @@
+import * as h3 from "h3-js";
+import { supportedCountries } from 'ssd-access';
+
+
+
+export function getCurrentLocation() {
+    return new Promise((resolve, reject) => {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const latAngle = position.coords.latitude;
+                const lonAngle = position.coords.longitude;
+
+                fetch(`https://nominatim.openstreetmap.org/reverse?
+                        lat=${latAngle}&lon=${lonAngle}&format=json&zoom=1&email=info%40michaelvogt.eu`)
+                    .then((response) => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            reject(response.text());
+                        }
+                    })
+                    .then((data) => {
+                        const countryCode = data.address.country_code;
+                        resolve({
+                            h3Index: h3.geoToH3(latAngle, lonAngle, 8),
+                            lat: latAngle,
+                            lon: lonAngle,
+                            regionCode: supportedCountries.includes(countryCode) ? countryCode : 'us'
+                        })
+                    })
+                    .catch((error) => {
+                        reject(error.message());
+                    });
+            }, (error) => {
+                console.log(`Location request failed: ${error}`)
+                reject(error);
+            }, {
+                enableHighAccuracy: false,
+                maximumAge: 0
+            });
+        } else {
+            reject('Location is not available');
+        }
+    });
+}
