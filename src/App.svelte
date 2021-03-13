ac6056ced7070c766b4e693e4e5560388a25fff2
Michael Vogt <info@michaelvogt.eu>
2021-03-13 11:00:43 +0100

    Svelte with Snowpack setup

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import {onMount} from 'svelte';
+
+  onMount(() => {
+  });
+</script>
+
+<style>
+  :global(body) {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
+
+<div>
+<h1>p2p test</h1>
+</div>
